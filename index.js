6847f371b2c1c4044d320c59587a89102887aeed
raojianbing <1151737656@qq.com>
2018-05-30 20:37:03 +0800

    express源码及依赖包

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-/*!
- * express
- * Copyright(c) 2009-2013 TJ Holowaychuk
- * Copyright(c) 2013 Roman Shtylman
- * Copyright(c) 2014-2015 Douglas Christopher Wilson
- * MIT Licensed
+/**
+ *user: raojianbing
+ *date: 2018-02-12 15:21
+ *description:
  */
+var express = require('express')
+var app = express()
 
-'use strict';
+app.all('/index', function (req, res) {
+	res.send('all /index request to homepage');
+});
 
-module.exports = require('./lib/express');
+app.get('/index', function (req, res) {
+	res.send('GET /index request to homepage');
+});
+
+app.get('/api/*', function (req, res) {
+	res.send('GET /api/* request to homepage');
+});
+
+app.post('/index', function (req, res) {
+	res.send('POST 1 /index request to homepage');
+});
+app.post('/index', function (req, res) {
+	res.send('POST 2 /index request to homepage');
+});
+
+
+
+
+console.log('===============')
+for(var i = 0; i < app._router.stack.length; i++){
+	console.log(app._router.stack[i])
+}
+console.log('===============')
+
+console.log(app._router)
